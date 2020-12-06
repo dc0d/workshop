3aa35f3aaabab2efc51c2cfaa430d925786b485e
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2020-12-06 21:48:01 +0200

    create the Golden Master from current behavior

--- workshop_test.go
+++ workshop_test.go
@@ -2,14 +2,18 @@ package workshop_test
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/dc0d/workshop"
+	"github.com/dc0d/workshop/test/support"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestUpdateQuality(t *testing.T) {
@@ -28,6 +32,9 @@ func TestUpdateQuality(t *testing.T) {
 			{
 				inputItem: workshop.Item{Name: "", SellIn: 0, Quality: 1},
 			},
+			{
+				inputItem: workshop.Item{Name: "", SellIn: -1, Quality: 2},
+			},
 		}
 	)
 
@@ -35,10 +42,10 @@ func TestUpdateQuality(t *testing.T) {
 		var (
 			inputItem      = exp.inputItem
 			expectedOutput workshop.Item
-			testCaseName   = filepath.Join("test", "fixture", fmt.Sprintf("%v-%v.json", t.Name(), index))
+			fixtureFile    = filepath.Join("test", "fixture", fmt.Sprintf("%v-%v.json", t.Name(), index))
 		)
 
-		fromJSON(readFile(testCaseName), &expectedOutput)
+		expectedOutput = getExpectedOutput(t, fixtureFile, inputItem)
 
 		input := []*workshop.Item{&inputItem}
 		workshop.UpdateQuality(input)
@@ -48,17 +55,53 @@ func TestUpdateQuality(t *testing.T) {
 	}
 }
 
-func readFile(p string) []byte {
-	content, err := ioutil.ReadFile(p)
+func getExpectedOutput(t *testing.T, fixtureFile string, item workshop.Item) (expectedOutput workshop.Item) {
+	js, err := readFile(fixtureFile)
 	if err != nil {
+		handled := false
+
+		if errors.Is(err, os.ErrNotExist) {
+			if *support.UpdateGoldenMaster {
+				handleGoldenMaster(t, fixtureFile, item)
+				handled = true
+			}
+		}
+
+		if !handled {
+			panic(err)
+		}
+	}
+	fromJSON(js, &expectedOutput)
+
+	return
+}
+
+func handleGoldenMaster(t *testing.T, fixtureFile string, item workshop.Item) {
+	input := []*workshop.Item{&item}
+	workshop.UpdateQuality(input)
+
+	actualOutput := item
+	js := toJSON(actualOutput)
+
+	if err := ioutil.WriteFile(fixtureFile, js, 0644); err != nil {
 		panic(err)
 	}
-	return content
+	require.FailNow(t, "Golden Master is written, run the test again without -update flag")
 }
+
+func readFile(p string) ([]byte, error) { return ioutil.ReadFile(p) }
 
 func fromJSON(js []byte, ptrVal interface{}) {
 	err := json.Unmarshal(js, ptrVal)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func toJSON(val interface{}) []byte {
+	js, err := json.Marshal(val)
+	if err != nil {
+		panic(err)
+	}
+	return js
 }
