5c7587846540affaa30761f1e2a449fc5677e79f
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2020-12-06 21:36:10 +0200

    first passing test

--- workshop.go
+++ workshop.go
@@ -1,54 +1,54 @@
 package workshop
 
 type Item struct {
-	name            string
-	sellIn, quality int
+	Name            string
+	SellIn, Quality int
 }
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
+		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+			if items[i].Quality > 0 {
+				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+					items[i].Quality = items[i].Quality - 1
 				}
 			}
 		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
+			if items[i].Quality < 50 {
+				items[i].Quality = items[i].Quality + 1
+				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
+					if items[i].SellIn < 11 {
+						if items[i].Quality < 50 {
+							items[i].Quality = items[i].Quality + 1
 						}
 					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
+					if items[i].SellIn < 6 {
+						if items[i].Quality < 50 {
+							items[i].Quality = items[i].Quality + 1
 						}
 					}
 				}
 			}
 		}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
+		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+			items[i].SellIn = items[i].SellIn - 1
 		}
 
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
+		if items[i].SellIn < 0 {
+			if items[i].Name != "Aged Brie" {
+				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+					if items[i].Quality > 0 {
+						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+							items[i].Quality = items[i].Quality - 1
 						}
 					}
 				} else {
-					items[i].quality = items[i].quality - items[i].quality
+					items[i].Quality = items[i].Quality - items[i].Quality
 				}
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
+				if items[i].Quality < 50 {
+					items[i].Quality = items[i].Quality + 1
 				}
 			}
 		}
--- workshop_test.go
+++ workshop_test.go
@@ -1,1 +1,19 @@
 package workshop_test
+
+import (
+	"testing"
+
+	"github.com/dc0d/workshop"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateQuality(t *testing.T) {
+	inputItem := workshop.Item{Name: "", SellIn: 0, Quality: 0}
+	expectedOutput := workshop.Item{Name: "", SellIn: -1, Quality: 0}
+
+	input := []*workshop.Item{&inputItem}
+	workshop.UpdateQuality(input)
+
+	actualOutput := inputItem
+	assert.Equal(t, expectedOutput, actualOutput)
+}
