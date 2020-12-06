4e079637bbc16d441a9652de5a2e4285dfcec3a8
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2020-12-06 21:36:12 +0200

    get the expected output from golden master files

--- workshop_test.go
+++ workshop_test.go
@@ -1,45 +1,64 @@
 package workshop_test
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/dc0d/workshop"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUpdateQuality(t *testing.T) {
-	type expectation struct {
-		inputItem      workshop.Item
-		expectedOutput workshop.Item
+	type testCase struct {
+		inputItem workshop.Item
 	}
 
 	var (
-		expectations = []expectation{
+		expectations = []testCase{
 			{
-				inputItem:      workshop.Item{Name: "", SellIn: 0, Quality: 0},
-				expectedOutput: workshop.Item{Name: "", SellIn: -1, Quality: 0},
+				inputItem: workshop.Item{Name: "", SellIn: 0, Quality: 0},
 			},
 			{
-				inputItem:      workshop.Item{Name: "NonExisting", SellIn: 0, Quality: 0},
-				expectedOutput: workshop.Item{Name: "NonExisting", SellIn: -1, Quality: 0},
+				inputItem: workshop.Item{Name: "NonExisting", SellIn: 0, Quality: 0},
 			},
 			{
-				inputItem:      workshop.Item{Name: "", SellIn: 0, Quality: 1},
-				expectedOutput: workshop.Item{Name: "", SellIn: -1, Quality: 0},
+				inputItem: workshop.Item{Name: "", SellIn: 0, Quality: 1},
 			},
 		}
 	)
 
-	for _, exp := range expectations {
+	for index, exp := range expectations {
 		var (
 			inputItem      = exp.inputItem
-			expectedOutput = exp.expectedOutput
+			expectedOutput workshop.Item
+			testCaseName   = filepath.Join("test", "fixture", fmt.Sprintf("%v-%v.json", t.Name(), index))
 		)
+
+		fromJSON(readFile(testCaseName), &expectedOutput)
 
 		input := []*workshop.Item{&inputItem}
 		workshop.UpdateQuality(input)
 
 		actualOutput := inputItem
 		assert.Equal(t, expectedOutput, actualOutput)
+	}
+}
+
+func readFile(p string) []byte {
+	content, err := ioutil.ReadFile(p)
+	if err != nil {
+		panic(err)
+	}
+	return content
+}
+
+func fromJSON(js []byte, ptrVal interface{}) {
+	err := json.Unmarshal(js, ptrVal)
+	if err != nil {
+		panic(err)
 	}
 }
