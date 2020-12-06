f40f403c4661a946fd3b20f762ed96f41687bd12
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2020-12-06 21:48:03 +0200

    separate the logic for Aged Brie

--- workshop.go
+++ workshop.go
@@ -7,49 +7,65 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
-				}
+		updateQuality(items[i])
+	}
+}
+
+func updateQuality(item *Item) {
+	if item.Name == "Aged Brie" {
+		if item.Quality < 50 {
+			item.Quality = item.Quality + 1
+		}
+
+		item.SellIn = item.SellIn - 1
+
+		if item.SellIn < 0 {
+			if item.Quality < 50 {
+				item.Quality = item.Quality + 1
 			}
-		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
+		}
+
+		return
+	}
+
+	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+		if item.Quality > 0 {
+			if item.Name != "Sulfuras, Hand of Ragnaros" {
+				item.Quality = item.Quality - 1
+			}
+		}
+	} else {
+		if item.Quality < 50 {
+			item.Quality = item.Quality + 1
+			if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+				if item.SellIn < 11 {
+					if item.Quality < 50 {
+						item.Quality = item.Quality + 1
 					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
+				}
+				if item.SellIn < 6 {
+					if item.Quality < 50 {
+						item.Quality = item.Quality + 1
 					}
 				}
 			}
 		}
+	}
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
-		}
+	if item.Name != "Sulfuras, Hand of Ragnaros" {
+		item.SellIn = item.SellIn - 1
+	}
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
+	if item.SellIn < 0 {
+		if item.Name != "Aged Brie" {
+			if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+				if item.Quality > 0 {
+					if item.Name != "Sulfuras, Hand of Ragnaros" {
+						item.Quality = item.Quality - 1
 					}
-				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
 				}
 			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
+				item.Quality = item.Quality - item.Quality
 			}
 		}
 	}
