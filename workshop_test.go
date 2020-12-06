df0f554a9719829a2969fb1eb1c22b41bab0e414
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2020-12-06 21:36:11 +0200

    add second and third cases
    change test to table driven

--- workshop_test.go
+++ workshop_test.go
@@ -8,12 +8,38 @@ import (
 )
 
 func TestUpdateQuality(t *testing.T) {
-	inputItem := workshop.Item{Name: "", SellIn: 0, Quality: 0}
-	expectedOutput := workshop.Item{Name: "", SellIn: -1, Quality: 0}
+	type expectation struct {
+		inputItem      workshop.Item
+		expectedOutput workshop.Item
+	}
 
-	input := []*workshop.Item{&inputItem}
-	workshop.UpdateQuality(input)
+	var (
+		expectations = []expectation{
+			{
+				inputItem:      workshop.Item{Name: "", SellIn: 0, Quality: 0},
+				expectedOutput: workshop.Item{Name: "", SellIn: -1, Quality: 0},
+			},
+			{
+				inputItem:      workshop.Item{Name: "NonExisting", SellIn: 0, Quality: 0},
+				expectedOutput: workshop.Item{Name: "NonExisting", SellIn: -1, Quality: 0},
+			},
+			{
+				inputItem:      workshop.Item{Name: "", SellIn: 0, Quality: 1},
+				expectedOutput: workshop.Item{Name: "", SellIn: -1, Quality: 0},
+			},
+		}
+	)
 
-	actualOutput := inputItem
-	assert.Equal(t, expectedOutput, actualOutput)
+	for _, exp := range expectations {
+		var (
+			inputItem      = exp.inputItem
+			expectedOutput = exp.expectedOutput
+		)
+
+		input := []*workshop.Item{&inputItem}
+		workshop.UpdateQuality(input)
+
+		actualOutput := inputItem
+		assert.Equal(t, expectedOutput, actualOutput)
+	}
 }
