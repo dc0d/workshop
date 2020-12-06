20fea924a23f8355cb35ddac920642d0bf1489c3
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2020-12-06 21:48:06 +0200

    refactor handling product types

--- workshop.go
+++ workshop.go
@@ -13,49 +13,64 @@ func UpdateQuality(items []*Item) {
 
 func updateQuality(item *Item) {
 	if item.Name == "Aged Brie" {
-		if item.Quality < 50 {
-			item.Quality = item.Quality + 1
-		}
-
-		item.SellIn = item.SellIn - 1
-
-		if item.SellIn < 0 {
-			if item.Quality < 50 {
-				item.Quality = item.Quality + 1
-			}
-		}
+		qualifyAgedBrie(item)
 
 		return
 	}
 
 	if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
-		if item.Quality < 50 {
-			item.Quality = item.Quality + 1
-			if item.SellIn < 11 {
-				if item.Quality < 50 {
-					item.Quality = item.Quality + 1
-				}
-			}
-			if item.SellIn < 6 {
-				if item.Quality < 50 {
-					item.Quality = item.Quality + 1
-				}
-			}
-		}
-
-		item.SellIn = item.SellIn - 1
-
-		if item.SellIn < 0 {
-			item.Quality = item.Quality - item.Quality
-		}
+		qualifyBackstagePassesToATafkal80EtcConcert(item)
 
 		return
 	}
 
 	if item.Name == "Sulfuras, Hand of Ragnaros" {
+		qualifySulfurasHandOfRagnaros(item)
 		return
 	}
 
+	defaultQualifier(item)
+}
+
+func qualifyAgedBrie(item *Item) {
+	if item.Quality < 50 {
+		item.Quality = item.Quality + 1
+	}
+
+	item.SellIn = item.SellIn - 1
+
+	if item.SellIn < 0 {
+		if item.Quality < 50 {
+			item.Quality = item.Quality + 1
+		}
+	}
+}
+
+func qualifyBackstagePassesToATafkal80EtcConcert(item *Item) {
+	if item.Quality < 50 {
+		item.Quality = item.Quality + 1
+		if item.SellIn < 11 {
+			if item.Quality < 50 {
+				item.Quality = item.Quality + 1
+			}
+		}
+		if item.SellIn < 6 {
+			if item.Quality < 50 {
+				item.Quality = item.Quality + 1
+			}
+		}
+	}
+
+	item.SellIn = item.SellIn - 1
+
+	if item.SellIn < 0 {
+		item.Quality = item.Quality - item.Quality
+	}
+}
+
+func qualifySulfurasHandOfRagnaros(item *Item) {}
+
+func defaultQualifier(item *Item) {
 	if item.Quality > 0 {
 		item.Quality = item.Quality - 1
 	}
