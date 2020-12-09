59c3680697bd9cc2afbdfecda5c9e2594c4717d4
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2020-12-09 21:19:48 +0200

    refactor qualifiers

--- workshop.go
+++ workshop.go
@@ -7,56 +7,64 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-		updateQuality(items[i])
+		qualifier := getQualifier(items[i].name)
+		qualifier.qualify(items[i])
 	}
 }
 
-func updateQuality(item *Item) {
-	if item.name == "Aged Brie" {
+func getQualifier(name string) qualifier {
+	switch name {
+	case "Aged Brie":
+		return qualifierFunc(qualifyAgedBrie)
+	case "Backstage passes to a TAFKAL80ETC concert":
+		return qualifierFunc(qualifyBackstagePassesToATafkal80EtcConcert)
+	case "Sulfuras, Hand of Ragnaros":
+		return qualifierFunc(qualifySulfurasHandOfRagnaros)
+	}
+
+	return qualifierFunc(qualifyUnknown)
+}
+
+func qualifyAgedBrie(item *Item) {
+	if item.quality < 50 {
+		item.quality = item.quality + 1
+	}
+
+	item.sellIn = item.sellIn - 1
+
+	if item.sellIn < 0 {
 		if item.quality < 50 {
 			item.quality = item.quality + 1
 		}
+	}
+}
 
-		item.sellIn = item.sellIn - 1
+func qualifyBackstagePassesToATafkal80EtcConcert(item *Item) {
+	if item.quality < 50 {
+		item.quality = item.quality + 1
 
-		if item.sellIn < 0 {
+		if item.sellIn < 11 {
 			if item.quality < 50 {
 				item.quality = item.quality + 1
 			}
 		}
-
-		return
-	}
-
-	if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-		if item.quality < 50 {
-			item.quality = item.quality + 1
-
-			if item.sellIn < 11 {
-				if item.quality < 50 {
-					item.quality = item.quality + 1
-				}
-			}
-			if item.sellIn < 6 {
-				if item.quality < 50 {
-					item.quality = item.quality + 1
-				}
+		if item.sellIn < 6 {
+			if item.quality < 50 {
+				item.quality = item.quality + 1
 			}
 		}
-
-		item.sellIn = item.sellIn - 1
-
-		if item.sellIn < 0 {
-			item.quality = item.quality - item.quality
-		}
-
-		return
 	}
 
-	if item.name == "Sulfuras, Hand of Ragnaros" {
-		return
-	}
+	item.sellIn = item.sellIn - 1
 
+	if item.sellIn < 0 {
+		item.quality = 0
+	}
+}
+
+func qualifySulfurasHandOfRagnaros(item *Item) {}
+
+func qualifyUnknown(item *Item) {
 	if item.quality > 0 {
 		item.quality = item.quality - 1
 	}
@@ -68,4 +76,12 @@ func updateQuality(item *Item) {
 			item.quality = item.quality - 1
 		}
 	}
+}
+
+type qualifierFunc func(item *Item)
+
+func (f qualifierFunc) qualify(item *Item) { f(item) }
+
+type qualifier interface {
+	qualify(item *Item)
 }
