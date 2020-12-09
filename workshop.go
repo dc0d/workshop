67fd985a04ea52648a48e86c4a57e9371b5014ef
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2020-12-09 21:19:38 +0200

    refactor logic branching for each item type

--- workshop.go
+++ workshop.go
@@ -7,50 +7,65 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
+		updateQuality(items[i])
+	}
+}
+
+func updateQuality(item *Item) {
+	if item.name == "Aged Brie" {
+		if item.quality < 50 {
+			item.quality = item.quality + 1
+		}
+
+		item.sellIn = item.sellIn - 1
+
+		if item.sellIn < 0 {
+			if item.quality < 50 {
+				item.quality = item.quality + 1
+			}
+		}
+
+		return
+	}
+
+	if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+		if item.quality < 50 {
+			item.quality = item.quality + 1
+
+			if item.sellIn < 11 {
+				if item.quality < 50 {
+					item.quality = item.quality + 1
 				}
 			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
+			if item.sellIn < 6 {
+				if item.quality < 50 {
+					item.quality = item.quality + 1
 				}
 			}
 		}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
+		item.sellIn = item.sellIn - 1
+
+		if item.sellIn < 0 {
+			item.quality = item.quality - item.quality
 		}
 
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
-			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
-			}
+		return
+	}
+
+	if item.name == "Sulfuras, Hand of Ragnaros" {
+		return
+	}
+
+	if item.quality > 0 {
+		item.quality = item.quality - 1
+	}
+
+	item.sellIn = item.sellIn - 1
+
+	if item.sellIn < 0 {
+		if item.quality > 0 {
+			item.quality = item.quality - 1
 		}
 	}
 }
