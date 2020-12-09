43e3f1bd2e749092631f8c406e1cdd2479ac1d61
Kaveh Shahbazian <kaveh.shahbazian@gmail.com>
2020-12-09 21:19:20 +0200

    first passing test

--- workshop_test.go
+++ workshop_test.go
@@ -1,1 +1,18 @@
-package workshop_test
+package workshop
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateQuality(t *testing.T) {
+	inputItem := Item{name: "", sellIn: 0, quality: 0}
+	expectedOutput := Item{name: "", sellIn: -1, quality: 0}
+
+	input := []*Item{&inputItem}
+	UpdateQuality(input)
+
+	actualOutput := inputItem
+	assert.Equal(t, expectedOutput, actualOutput)
+}
